e98114b83806dbeb9e7b05be32f4e9282c4ab4e9
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-10-12 04:06:37 +0800

    done with ex01

--- ex01/ft_list_push_front.c
+++ ex01/ft_list_push_front.c
@@ -6,7 +6,33 @@
 /*   By: jegoh <jegoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 03:41:17 by jegoh             #+#    #+#             */
-/*   Updated: 2024/10/12 03:41:18 by jegoh            ###   ########.fr       */
+/*   Updated: 2024/10/12 04:06:11 by jegoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_list.h"
+#include <stdlib.h>
+
+t_list *ft_create_elem(void *data)
+{
+    t_list *new_elem;
+
+    new_elem = (t_list *)malloc(sizeof(t_list));
+    if (new_elem == NULL)
+        return (NULL);
+    new_elem->data = data;
+    new_elem->next = NULL;
+    return (new_elem);
+}
+
+void ft_list_push_front(t_list **begin_list, void *data)
+{
+    t_list *new_elem;
+
+    new_elem = ft_create_elem(data);
+    if (new_elem == NULL)
+        return;
+
+    new_elem->next = *begin_list;
+    *begin_list = new_elem;
+}
