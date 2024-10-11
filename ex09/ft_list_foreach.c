7a0cbe67eba787c0e0883e814b16d4cd00bd8412
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-10-12 06:08:12 +0800

    done with ex09

--- ex09/ft_list_foreach.c
+++ ex09/ft_list_foreach.c
@@ -6,7 +6,20 @@
 /*   By: jegoh <jegoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 03:43:04 by jegoh             #+#    #+#             */
-/*   Updated: 2024/10/12 03:43:05 by jegoh            ###   ########.fr       */
+/*   Updated: 2024/10/12 06:08:06 by jegoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_list.h"
+
+void ft_list_foreach(t_list *begin_list, void (*f)(void *))
+{
+    t_list *current;
+
+    current = begin_list;
+    while (current)
+    {
+        (*f)(current->data);
+        current = current->next;
+    }
+}
