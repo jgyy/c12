680d942b76da03d0cd3d025e6c1d44dae65c2799
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-10-12 04:03:57 +0800

    done with ex00

--- ex00/ft_create_elem.c
+++ ex00/ft_create_elem.c
@@ -6,7 +6,21 @@
 /*   By: jegoh <jegoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 03:41:01 by jegoh             #+#    #+#             */
-/*   Updated: 2024/10/12 03:41:02 by jegoh            ###   ########.fr       */
+/*   Updated: 2024/10/12 04:03:41 by jegoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_list.h"
+#include <stdlib.h>
+
+t_list *ft_create_elem(void *data)
+{
+    t_list *new_elem;
+
+    new_elem = (t_list *)malloc(sizeof(t_list));
+    if (new_elem == NULL)
+        return (NULL);
+    new_elem->data = data;
+    new_elem->next = NULL;
+    return (new_elem);
+}
--- ex00/ft_list.h
+++ ex00/ft_list.h
@@ -6,7 +6,19 @@
 /*   By: jegoh <jegoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 03:40:14 by jegoh             #+#    #+#             */
-/*   Updated: 2024/10/12 03:40:15 by jegoh            ###   ########.fr       */
+/*   Updated: 2024/10/12 04:03:31 by jegoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_LIST_H
+#define FT_LIST_H
+
+typedef struct s_list
+{
+    struct s_list *next;
+    void *data;
+} t_list;
+
+t_list *ft_create_elem(void *data);
+
+#endif
