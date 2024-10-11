b496d018e310066ff5c3e699ad0823a54a0aa76b
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-10-12 06:31:12 +0800

    ex12 is done

--- ex12/ft_list_remove_if.c
+++ ex12/ft_list_remove_if.c
@@ -6,7 +6,43 @@
 /*   By: jegoh <jegoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 04:00:29 by jegoh             #+#    #+#             */
-/*   Updated: 2024/10/12 04:00:30 by jegoh            ###   ########.fr       */
+/*   Updated: 2024/10/12 06:31:03 by jegoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_list.h"
+#include <stdlib.h>
+
+void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)(), void (*free_fct)(void *))
+{
+    t_list *current;
+    t_list *prev;
+    t_list *temp;
+
+    if (!begin_list || !*begin_list)
+        return;
+
+    current = *begin_list;
+    prev = NULL;
+
+    while (current)
+    {
+        if ((*cmp)(current->data, data_ref) == 0)
+        {
+            if (prev)
+                prev->next = current->next;
+            else
+                *begin_list = current->next;
+
+            temp = current;
+            current = current->next;
+            (*free_fct)(temp->data);
+            free(temp);
+        }
+        else
+        {
+            prev = current;
+            current = current->next;
+        }
+    }
+}
