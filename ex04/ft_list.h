8c8c64f703443f7dbb7ac8a02340c753456b5124
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-10-12 04:30:11 +0800

    done with ex04

--- ex04/ft_list.h
+++ ex04/ft_list.h
@@ -6,7 +6,20 @@
 /*   By: jegoh <jegoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 03:40:14 by jegoh             #+#    #+#             */
-/*   Updated: 2024/10/12 03:40:15 by jegoh            ###   ########.fr       */
+/*   Updated: 2024/10/12 04:29:42 by jegoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_LIST_H
+#define FT_LIST_H
+
+typedef struct s_list
+{
+    struct s_list *next;
+    void *data;
+} t_list;
+
+t_list *ft_create_elem(void *data);
+void ft_list_push_back(t_list **begin_list, void *data);
+
+#endif
--- ex04/ft_list_push_back.c
+++ ex04/ft_list_push_back.c
@@ -6,7 +6,38 @@
 /*   By: jegoh <jegoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 03:41:53 by jegoh             #+#    #+#             */
-/*   Updated: 2024/10/12 03:41:54 by jegoh            ###   ########.fr       */
+/*   Updated: 2024/10/12 04:30:03 by jegoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_list.h"
+#include <stdlib.h>
+
+t_list *ft_create_elem(void *data)
+{
+    t_list *new_elem;
+
+    new_elem = (t_list *)malloc(sizeof(t_list));
+    if (new_elem == NULL)
+        return (NULL);
+    new_elem->data = data;
+    new_elem->next = NULL;
+    return (new_elem);
+}
+
+void ft_list_push_back(t_list **begin_list, void *data)
+{
+    t_list *new_elem;
+    t_list *current;
+
+    new_elem = ft_create_elem(data);
+    if (*begin_list == NULL)
+    {
+        *begin_list = new_elem;
+        return;
+    }
+    current = *begin_list;
+    while (current->next != NULL)
+        current = current->next;
+    current->next = new_elem;
+}
