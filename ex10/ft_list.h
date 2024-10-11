7239edc058cfbac2638da40a61d31291ad11fd2b
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-10-12 06:20:26 +0800

    ex10 is done

--- ex10/ft_list.h
+++ ex10/ft_list.h
@@ -6,7 +6,20 @@
 /*   By: jegoh <jegoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 03:40:14 by jegoh             #+#    #+#             */
-/*   Updated: 2024/10/12 03:40:15 by jegoh            ###   ########.fr       */
+/*   Updated: 2024/10/12 06:18:59 by jegoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_LIST_H
+#define FT_LIST_H
+
+typedef struct s_list
+{
+    struct s_list *next;
+    void *data;
+} t_list;
+
+t_list *ft_create_elem(void *data);
+void ft_list_foreach_if(t_list *begin_list, void (*f)(void *), void *data_ref, int (*cmp)());
+
+#endif
--- ex10/ft_list_foreach_if.c
+++ ex10/ft_list_foreach_if.c
@@ -6,7 +6,36 @@
 /*   By: jegoh <jegoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 03:43:19 by jegoh             #+#    #+#             */
-/*   Updated: 2024/10/12 03:43:20 by jegoh            ###   ########.fr       */
+/*   Updated: 2024/10/12 06:19:51 by jegoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_list.h"
+#include <stdlib.h>
+
+t_list *ft_create_elem(void *data)
+{
+    t_list *new_elem;
+
+    new_elem = (t_list *)malloc(sizeof(t_list));
+    if (new_elem == NULL)
+        return (NULL);
+    new_elem->data = data;
+    new_elem->next = NULL;
+    return (new_elem);
+}
+
+void ft_list_foreach_if(t_list *begin_list, void (*f)(void *), void *data_ref, int (*cmp)())
+{
+    t_list *current;
+
+    current = begin_list;
+    while (current != NULL)
+    {
+        if ((*cmp)(current->data, data_ref) == 0)
+        {
+            (*f)(current->data);
+        }
+        current = current->next;
+    }
+}
