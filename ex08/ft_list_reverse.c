87b7b8fc848873c106a5686d6d73425c14372abc
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-10-12 05:55:19 +0800

    done with ex08

--- ex08/ft_list_reverse.c
+++ ex08/ft_list_reverse.c
@@ -6,7 +6,25 @@
 /*   By: jegoh <jegoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 03:42:51 by jegoh             #+#    #+#             */
-/*   Updated: 2024/10/12 03:42:52 by jegoh            ###   ########.fr       */
+/*   Updated: 2024/10/12 05:55:13 by jegoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_list.h"
+#include <stdlib.h>
+
+void ft_list_reverse(t_list **begin_list)
+{
+    t_list *prev = NULL;
+    t_list *current = *begin_list;
+    t_list *next = NULL;
+
+    while (current != NULL)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    *begin_list = prev;
+}
