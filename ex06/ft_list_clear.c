210e016825fdabe8e19f8a4c6c809a876e84cfa1
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-10-12 05:03:11 +0800

    done with ex06

--- ex06/ft_list_clear.c
+++ ex06/ft_list_clear.c
@@ -6,7 +6,25 @@
 /*   By: jegoh <jegoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 03:42:17 by jegoh             #+#    #+#             */
-/*   Updated: 2024/10/12 03:42:18 by jegoh            ###   ########.fr       */
+/*   Updated: 2024/10/12 05:02:54 by jegoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_list.h"
+#include <stdlib.h>
+
+void ft_list_clear(t_list *begin_list, void (*free_fct)(void *))
+{
+    t_list *current;
+    t_list *next;
+
+    current = begin_list;
+    while (current)
+    {
+        next = current->next;
+        if (free_fct)
+            free_fct(current->data);
+        free(current);
+        current = next;
+    }
+}
