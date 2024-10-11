bf4101a3ffce829410ea79ec9c8cf0f80a4b4df8
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-10-12 05:40:04 +0800

    done with ex07

--- ex07/ft_list.h
+++ ex07/ft_list.h
@@ -6,7 +6,19 @@
 /*   By: jegoh <jegoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 03:40:14 by jegoh             #+#    #+#             */
-/*   Updated: 2024/10/12 03:40:15 by jegoh            ###   ########.fr       */
+/*   Updated: 2024/10/12 05:39:12 by jegoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_LIST_H
+#define FT_LIST_H
+
+typedef struct s_list
+{
+    struct s_list *next;
+    void *data;
+} t_list;
+
+t_list *ft_list_at(t_list *begin_list, unsigned int nbr);
+
+#endif
--- ex07/ft_list_at.c
+++ ex07/ft_list_at.c
@@ -6,7 +6,30 @@
 /*   By: jegoh <jegoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 03:42:30 by jegoh             #+#    #+#             */
-/*   Updated: 2024/10/12 03:42:31 by jegoh            ###   ########.fr       */
+/*   Updated: 2024/10/12 05:39:21 by jegoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_list.h"
+
+t_list *ft_list_at(t_list *begin_list, unsigned int nbr)
+{
+    t_list *current;
+    unsigned int i;
+
+    if (begin_list == 0)
+        return (0);
+
+    current = begin_list;
+    i = 0;
+
+    while (current != 0)
+    {
+        if (i == nbr)
+            return (current);
+        current = current->next;
+        i++;
+    }
+
+    return (0);
+}
