0c9346c16aa7ab05d7a2bbcc2f30c5c0353cb1b6
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-10-12 06:37:39 +0800

    ex14 is done

--- ex13/ft_list_merge.c
+++ ex13/ft_list_merge.c
@@ -6,11 +6,12 @@
 /*   By: jegoh <jegoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 04:00:44 by jegoh             #+#    #+#             */
-/*   Updated: 2024/10/12 06:36:06 by jegoh            ###   ########.fr       */
+/*   Updated: 2024/10/12 06:36:26 by jegoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_list.h"
+#include <stdlib.h>
 
 void ft_list_merge(t_list **begin_list1, t_list *begin_list2)
 {
--- ex14/ft_list_sort.c
+++ ex14/ft_list_sort.c
@@ -6,7 +6,43 @@
 /*   By: jegoh <jegoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 04:00:54 by jegoh             #+#    #+#             */
-/*   Updated: 2024/10/12 04:00:55 by jegoh            ###   ########.fr       */
+/*   Updated: 2024/10/12 06:37:34 by jegoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_list.h"
+#include <stdlib.h>
+
+static void swap_nodes(t_list *a, t_list *b)
+{
+    void *temp = a->data;
+    a->data = b->data;
+    b->data = temp;
+}
+
+void ft_list_sort(t_list **begin_list, int (*cmp)())
+{
+    int swapped;
+    t_list *ptr1;
+    t_list *lptr = NULL;
+
+    if (begin_list == NULL || *begin_list == NULL)
+        return;
+
+    do
+    {
+        swapped = 0;
+        ptr1 = *begin_list;
+
+        while (ptr1->next != lptr)
+        {
+            if ((*cmp)(ptr1->data, ptr1->next->data) > 0)
+            {
+                swap_nodes(ptr1, ptr1->next);
+                swapped = 1;
+            }
+            ptr1 = ptr1->next;
+        }
+        lptr = ptr1;
+    } while (swapped);
+}
