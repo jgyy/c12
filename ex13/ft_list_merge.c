52ad55c2d7a59aca757792d95ff489513e284d7e
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-10-12 06:36:13 +0800

    ex13 is done

--- ex13/ft_list_merge.c
+++ ex13/ft_list_merge.c
@@ -6,7 +6,27 @@
 /*   By: jegoh <jegoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 04:00:44 by jegoh             #+#    #+#             */
-/*   Updated: 2024/10/12 04:00:45 by jegoh            ###   ########.fr       */
+/*   Updated: 2024/10/12 06:36:06 by jegoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_list.h"
+
+void ft_list_merge(t_list **begin_list1, t_list *begin_list2)
+{
+    t_list *current;
+
+    if (*begin_list1 == NULL)
+    {
+        *begin_list1 = begin_list2;
+        return;
+    }
+
+    current = *begin_list1;
+    while (current->next != NULL)
+    {
+        current = current->next;
+    }
+
+    current->next = begin_list2;
+}
