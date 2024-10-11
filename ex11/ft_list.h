12fbca7c23cf36eb7a707ca2ac6abf81d2e74dc9
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-10-12 06:26:52 +0800

    ex11 is done

--- ex11/ft_list.h
+++ ex11/ft_list.h
@@ -6,7 +6,19 @@
 /*   By: jegoh <jegoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 03:40:14 by jegoh             #+#    #+#             */
-/*   Updated: 2024/10/12 03:40:15 by jegoh            ###   ########.fr       */
+/*   Updated: 2024/10/12 06:26:40 by jegoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_LIST_H
+#define FT_LIST_H
+
+typedef struct s_list
+{
+    struct s_list *next;
+    void          *data;
+} t_list;
+
+t_list *ft_list_find(t_list *begin_list, void *data_ref, int (*cmp)());
+
+#endif
--- ex11/ft_list_find.c
+++ ex11/ft_list_find.c
@@ -6,7 +6,22 @@
 /*   By: jegoh <jegoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 03:43:29 by jegoh             #+#    #+#             */
-/*   Updated: 2024/10/12 03:43:30 by jegoh            ###   ########.fr       */
+/*   Updated: 2024/10/12 06:26:47 by jegoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_list.h"
+
+t_list *ft_list_find(t_list *begin_list, void *data_ref, int (*cmp)())
+{
+    t_list *current;
+
+    current = begin_list;
+    while (current)
+    {
+        if ((*cmp)(current->data, data_ref) == 0)
+            return (current);
+        current = current->next;
+    }
+    return (NULL);
+}
