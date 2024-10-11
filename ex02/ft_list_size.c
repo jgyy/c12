aa5fde3367aa781d6fa07d79a722576f25a27566
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-10-12 04:16:30 +0800

    done with ex02

--- ex02/ft_list_size.c
+++ ex02/ft_list_size.c
@@ -6,7 +6,22 @@
 /*   By: jegoh <jegoh@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/12 03:41:29 by jegoh             #+#    #+#             */
-/*   Updated: 2024/10/12 03:41:30 by jegoh            ###   ########.fr       */
+/*   Updated: 2024/10/12 04:16:25 by jegoh            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_list.h"
+
+int ft_list_size(t_list *begin_list)
+{
+    int count = 0;
+    t_list *current = begin_list;
+
+    while (current != NULL)
+    {
+        count++;
+        current = current->next;
+    }
+
+    return count;
+}
